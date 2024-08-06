3e63997a769186f047e822adf312df10f7f30f8c
Ola Rasmus Moen <olamoen@protonmail.com>
2024-08-06 17:12:54 +0200

    remove comments/redndant code

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ import pandas as pd
 
 from data import get_data
 
-df2 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 df = get_data()
 
 app = Dash()
 
 app.layout = [
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    #dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Dropdown(df.keys()[2:], 'T_out', id='dropdown-selection'),
     dcc.Graph(id='graph-content'),
     dcc.DatePickerSingle(
@@ -36,23 +34,14 @@ app.layout = [
 ]
 
 @callback(
-    Output('graph-content', 'figure'),# Output('output-container-date-picker-single', 'children')],
+    Output('graph-content', 'figure'),
     [Input('dropdown-selection', 'value'), Input('date-from', 'date'), Input('date-to', 'date')]
 )
 def update_graph(value, date_from, date_to):
     date_from = datetime.strptime(date_from, "%Y-%m-%d").date()
     date_to = datetime.strptime(date_to, "%Y-%m-%d").date()
     df = get_data(date_from)
-    #dff = df[df.country==value]
-    #return px.line(dff, x='year', y='pop')
     return px.line(df, x='timestamp', y=value)
-
-#@callback(
-#    Output('output-container-date-picker-single', 'children'),
-#    Input('date-from', 'date')
-#)
-#def update_output(selected_date):
-#    return f'Selected date: {selected_date}'
 
 if __name__ == '__main__':
     app.run(debug=True)
