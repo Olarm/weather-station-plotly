6451bfe9be506680b50588c46365a11478e377e0
Ola Rasmus Moen <olamoen@protonmail.com>
2024-08-07 11:55:41 +0200

    Different setup depending on environment and improve visualizaton of date pickers

--- app.py
+++ app.py
@@ -1,38 +1,51 @@
 from datetime import date, datetime, timedelta
+import os
 
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-
 from data import get_data
+
+
+PRODUCTION = False
+if os.getenv('FLASK_ENV') == 'development':
+    print("Running in development mode")
+elif os.getenv('FLASK_ENV') == 'production':
+    print("Running in production mode with Gunicorn")
+    PRODUCTION = True
 
 df = get_data()
 
 app = Dash()
+server = app.server
 app.title = "Rennodden"
 
 
 app.layout = [
     html.H1(children='Rennodden værstasjon ved Dalskilen', style={'textAlign':'center'}),
     dcc.Dropdown(df.keys()[2:], 'Temperatur ute', id='dropdown-selection'),
+    html.Div(['Fra: ',
+        dcc.DatePickerSingle(
+            month_format='Do, MMM YY',
+            placeholder='Do, MMM YY',
+            min_date_allowed=date(2022, 1, 1),
+            max_date_allowed=date.today(),
+            date=date.today() - timedelta(days=1),
+            id='date-from'
+        ),
+        "Til: ",
+        dcc.DatePickerSingle(
+            month_format='Do, MMM YY',
+            placeholder='Do, MMM YY',
+            min_date_allowed=date(2022, 1, 1),
+            max_date_allowed=date.today(),
+            date=date.today() - timedelta(days=1),
+            id='date-to'
+        )],
+        style={'fontSize': 20},
+    ),
     dcc.Graph(id='graph-content'),
-    dcc.DatePickerSingle(
-        month_format='Do, MMM YY',
-        placeholder='Do, MMM YY',
-        min_date_allowed=date(2022, 1, 1),
-        max_date_allowed=date.today(),
-        date=date.today() - timedelta(days=1),
-        id='date-from'
-    ),
-    dcc.DatePickerSingle(
-        month_format='Do, MMM YY',
-        placeholder='Do, MMM YY',
-        min_date_allowed=date(2022, 1, 1),
-        max_date_allowed=date.today(),
-        date=date.today() - timedelta(days=1),
-        id='date-to'
-    ),
 ]
 
 @callback(
@@ -46,4 +59,4 @@ def update_graph(value, date_from, date_to):
     return px.line(df, x='timestamp', y=value)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=PRODUCTION)
